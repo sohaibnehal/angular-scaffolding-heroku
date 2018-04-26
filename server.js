8e0a05a3e6e049f5b12656b805fd2320883389ac
Sohaib Nehal <sohaib.nehal@ymail.com>
2018-04-26 12:47:52 +0500

    Updating server

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
 
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Send all other requests to the Angular app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './dist')));
+
+// Serving client side app.
+
+app.get('/', (request, response) => {
+  response.sendFile(path.resolve(__dirname, './dist', 'index.html'));
 });
 
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
